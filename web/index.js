7efb3fb052cfd09c17a146bdfa175188b0c31462
Taufik Nurrohman <hi@taufik-nurrohman.com>
2026-05-03 14:55:23 +0700

    Update

--- web/index.js
+++ web/index.js
@@ -1,9 +1,4 @@
-(() => {
-
-const hub = 'http://127.0.0.1/test/hub';
-const sub = '/peer/web';
-
-let route = window.location.pathname.slice(sub.length);
+// (() => {
 
 const form = {};
 const formAlert = document.createElement('p');
@@ -35,11 +30,7 @@ form.user.addEventListener('submit', function (e) {
         method: 'POST'
     }).then(r => r.json()).then(r => {
         if (200 !== r.status) {
-            if (401 === r.status) {
-                formAlert.innerHTML = 'Invalid user or pass.';
-            } else {
-                formAlert.innerHTML = 'Unknown error.';
-            }
+            formAlert.innerHTML = r.description || 'Unknown error.';
             this.elements.pass.value = "";
             this.elements.pass.focus();
             this.prepend(formAlert);
@@ -49,7 +40,8 @@ form.user.addEventListener('submit', function (e) {
         // similar method(s). This practice is only for demonstration and educational purpose(s).
         localStorage.setItem('jwt', r.token);
         localStorage.setItem('user', r.user);
-        displayEntries('/asset');
+        window.history.pushState({}, "", sub + '/lot/asset?chunk=5&part=1');
+        display(1);
     }).catch(e => {
         formAlert.innerHTML = e;
         this.prepend(formAlert);
@@ -57,45 +49,136 @@ form.user.addEventListener('submit', function (e) {
     e.preventDefault();
 });
 
-function displayEntries(of, part = 1) {
-    const description = document.createElement('p');
-    const list = document.createElement('ul');
-    description.innerHTML = 'Loading…';
-    description.setAttribute('role', 'status');
-    view.replaceChildren(description);
-    request(hub + '/get/data' + of + '?chunk=20&part=' + part).then(r => r.json()).then(r => {
-        console.log(r);
-        document.title = 'Entries';
-        window.history.pushState({}, "", sub + of + '?part=' + part);
-        if (0 === r.data.total) {
-            description.innerHTML = 'No entries yet.';
+function display(status) {
+    const hash = window.location.hash;
+    const path = window.location.pathname.slice(sub.length);
+    const query = Object.fromEntries(new URLSearchParams(window.location.search));
+    if ('/user' === path) {
+        if (localStorage.getItem('jwt')) {
+            // TODO: Persistent enter state
+            displayFormUser(status);
         } else {
-            view.replaceChildren(list);
-            // Custom sort, to prioritize folder
-            r.data.lot.sort((a, b) => {
-                if (a.is.folder !== b.is.folder) {
-                    return a.is.folder ? -1 : 1;
-                }
-                return a.route.localeCompare(b.route);
-            });
-            r.data.lot.forEach(v => {
-                const listItem = document.createElement('li');
-                const listItemLink = document.createElement('a');
-                const listItemLinks = document.createElement('span');
-                listItemLink.href = "";
-                listItemLink.innerHTML = v.route.slice(1) + (v.is.file ? "" : '/');
-                listItem.append(v.is.file ? '📄 ' : '📁 ');
-                listItem.append(listItemLink);
-                listItem.append(listItemLinks);
-                list.append(listItem);
-            });
+            displayFormUser(status);
+        }
+    } else {
+        query._status = status;
+        query.part ? displayLotItems(path, query, hash) : displayLotItem(path, query, hash);
+    }
+}
+
+function displayLotItem(path, query, hash) {
+    const description = document.createElement('p');
+    const itemContent = document.createElement('pre');
+    const itemTitle = document.createElement('h1');
+    description.innerHTML = 'Loading…';
+    description.setAttribute('role', 'alert');
+    view.replaceChildren(description);
+    request(hub + '/get/data' + path.slice(4)).then(r => r.json()).then(r => {
+        console.log(r);
+        if (404 === r.status) {
+
+        } else {
+            document.title = 'Application · `.' + path + '`';
+            itemContent.textContent = JSON.stringify(r, null, 2);
+            itemTitle.innerHTML = '.' + path;
+            view.replaceChildren(itemTitle, itemContent);
+            onAfterDisplay();
         }
     }).catch(console.error);
 }
 
-function displayFormUser() {
-    document.title = 'Enter';
-    window.history.pushState({}, "", sub + '/user');
+function displayLotItems(path, query, hash) {
+    const description = document.createElement('p');
+    const listItems = document.createElement('ul');
+    const listNav = document.createElement('nav');
+    const listNavLinkNext = document.createElement('a');
+    const listNavLinkParent = document.createElement('a');
+    const listNavLinkPrev = document.createElement('a');
+    const listTitle = document.createElement('h1');
+    listNavLinkNext.innerHTML = '➡️';
+    listNavLinkNext.title = 'Go to the next page';
+    listNavLinkParent.innerHTML = '⬆️';
+    listNavLinkParent.title = 'Go to parent';
+    listNavLinkPrev.innerHTML = '⬅️';
+    listNavLinkPrev.title = 'Go to the previous page';
+    description.innerHTML = 'Loading…';
+    description.setAttribute('role', 'alert');
+    view.replaceChildren(description);
+    request(hub + '/get/data' + path.slice(4) + '?chunk=' + query.chunk + '&part=' + query.part).then(r => r.json()).then(r => {
+        console.log(r);
+        document.title = 'Application · `.' + path + '`';
+        if (!r.has.children) {
+            description.innerHTML = 'No items yet.';
+        } else {
+            listNavLinkNext.setAttribute('href', sub + r.data.route + '?chunk=' + r.query.chunk + '&part=' + (r.query.part + 1));
+            listNavLinkParent.setAttribute('href', sub + r.data.parent.route + '?chunk=' + r.query.chunk + '&part=' + r.query.part);
+            listNavLinkPrev.setAttribute('href', sub + r.data.route + '?chunk=' + r.query.chunk + '&part=' + (r.query.part - 1));
+            listNavLinkNext.addEventListener('click', function (e) {
+                window.history.pushState({}, "", this.getAttribute('href'));
+                display();
+                e.preventDefault();
+            });
+            listNavLinkParent.addEventListener('click', function (e) {
+                window.history.pushState({}, "", this.getAttribute('href'));
+                display();
+                e.preventDefault();
+            });
+            listNavLinkPrev.addEventListener('click', function (e) {
+                window.history.pushState({}, "", this.getAttribute('href'));
+                display();
+                e.preventDefault();
+            });
+            if (r.has.next) {
+                listNavLinkNext.removeAttribute('aria-disabled');
+            } else {
+                listNavLinkNext.setAttribute('aria-disabled', 'true');
+            }
+            if (r.has.parent) {
+                listNavLinkParent.removeAttribute('aria-disabled');
+            } else {
+                listNavLinkParent.setAttribute('aria-disabled', 'true');
+            }
+            if (r.has.prev) {
+                listNavLinkPrev.removeAttribute('aria-disabled');
+            } else {
+                listNavLinkPrev.setAttribute('aria-disabled', 'true');
+            }
+            listNav.append(listNavLinkPrev, ' ', listNavLinkParent, ' ', listNavLinkNext);
+            listTitle.innerHTML = '.' + path + '#' + r.query.part;
+            view.replaceChildren(listTitle, listItems, listNav);
+            r.data.children.forEach(v => {
+                const listItem = document.createElement('li');
+                const listItemLink = document.createElement('a');
+                const listItemLinks = document.createElement('span');
+                const listItemSize = document.createElement('span');
+                listItemSize.innerHTML = v.size;
+                listItemSize.setAttribute('role', 'status');
+                listItemLink.innerHTML = v.name + (v.is.file ? '.' + v.x : '/');
+                if (v.is.blob) {
+                    listItemLink.addEventListener('click', function (e) {
+                        openBlob(this.getAttribute('href'));
+                        e.preventDefault();
+                    });
+                    listItemLink.href = hub + '/get/blob' + v.route.slice(4);
+                    listItemLink.innerHTML += ' ↗️';
+                } else {
+                    listItemLink.addEventListener('click', function (e) {
+                        window.history.pushState({}, "", this.getAttribute('href'));
+                        display();
+                        e.preventDefault();
+                    });
+                    listItemLink.href = sub + v.route + (v.is.folder ? '?chunk=' + query.chunk + '&part=1' : "");
+                }
+                listItem.append(v.is.file ? '📄 ' : '📁 ', listItemLink, ' ', listItemSize, listItemLinks);
+                listItems.append(listItem);
+            });
+            onAfterDisplay();
+        }
+    }).catch(console.error);
+}
+
+function displayFormUser(status) {
+    document.title = 'Application · Enter';
     const key = document.createElement('input');
     const keyParent = document.createElement('p');
     const pass = document.createElement('input');
@@ -120,24 +203,65 @@ function displayFormUser() {
     taskParent.setAttribute('role', 'group');
     form.user.replaceChildren(keyParent, passParent, taskParent, peer);
     view.replaceChildren(form.user);
+    key.focus();
+    if (-1 === status && !localStorage.getItem('jwt')) {
+        const description = document.createElement('p');
+        description.innerHTML = 'Logged out.';
+        description.setAttribute('role', 'alert');
+        view.prepend(description);
+    }
 }
 
-function request(of, method = 'GET', headers = {}, body = "") {
+function onAfterDisplay() {
+    // Insert exit link
+    const exit = document.createElement('button');
+    const p = document.createElement('p');
+    exit.addEventListener('click', function (e) {
+        localStorage.removeItem('jwt');
+        localStorage.removeItem('user');
+        window.history.pushState({}, "", sub + '/user');
+        display(-1);
+        e.preventDefault();
+    });
+    exit.innerHTML = 'Exit';
+    p.append(exit);
+    view.append(p);
+    // if (1 === query._status) {
+    //     const description = document.createElement('p');
+    //     description.innerHTML = 'Logged in.';
+    //     description.setAttribute('role', 'alert');
+    //     view.prepend(description);
+    // }
+}
+
+function openBlob(path, query, hash) {
+    request(path, 'GET').then(r => {
+        if (!r.ok) {
+            throw new Error('Request failed.');
+        }
+        return r.blob();
+    }).then(blob => {
+        let v = URL.createObjectURL(blob);
+        window.open(v, '_blank');
+        setTimeout(() => URL.revokeObjectURL(v), 1000);
+    }).catch(console.error);
+}
+
+function request(path, method = 'GET', headers = {}, body = "") {
     const jwt = localStorage.getItem('jwt');
-    headers = Object.assign(headers, {
+    headers = Object.assign({
         'authorization': 'bearer ' + jwt,
         'content-type': 'application/json'
-    });
-    return fetch(of, 'GET' === method || 'HEAD' === method ? { headers, method } : { body, headers, method });
+    }, headers);
+    return fetch(path, 'GET' === method || 'HEAD' === method ? { headers, method } : { body, headers, method });
 }
 
-displayFormUser();
+if ('/' !== window.location.pathname.slice(sub.length) || "" !== window.location.search) {} else {
+    window.history.pushState({}, "", sub + '/user');
+}
 
-console.log(route);
+display();
 
-window.addEventListener('popstate', function (e) {
-    route = window.location.pathname.slice(sub.length);
-    console.log(route);
-});
+window.addEventListener('popstate', display);
 
-})();
+// })();
--- web/index.php
+++ web/index.php
@@ -1,14 +1,24 @@
+<?php
+
+$hub = 'http://127.0.0.1/test/hub';
+$sub = '/peer/web';
+
+?>
 <!DOCTYPE html>
 <html dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Loading…</title>
-    <link href="index.css" rel="stylesheet">
+    <link href="<?= $sub; ?>/index.css" rel="stylesheet">
   </head>
   <body>
     <div role="application">
       <p role="status">Loading…</p>
     </div>
-    <script src="index.js"></script>
+    <script>
+      const hub = '<?= $hub; ?>';
+      const sub = '<?= $sub; ?>';
+    </script>
+    <script src="<?= $sub; ?>/index.js"></script>
   </body>
 </html>
